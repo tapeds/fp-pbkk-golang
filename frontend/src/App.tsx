f7115f18d751763a4b44573d6055eb38f0b7b2d8
tapeds <frrllmatthew@gmail.com>
2024-12-08 22:52:10 +0700

    feat: add arah penerbangan

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import AdminPage from "./pages/admin/AdminPage";
+import AdminPenerbangan from "./pages/admin/AdminPenerbanganPage";
+import AdminMaskapai from "./pages/admin/AdminMaskapaiPage";
+import AdminBandara from "./pages/admin/AdminBandaraPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/admin" element={<AdminPage />} />
+      <Route path="/admin/penerbangan" element={<AdminPenerbangan />} />
+      <Route path="/admin/maskapai" element={<AdminMaskapai />} />
+      <Route path="/admin/bandara" element={<AdminBandara />} />
     </Routes>
   );
 }
